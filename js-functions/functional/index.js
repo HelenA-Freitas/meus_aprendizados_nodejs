100d38a5a49033ffac0ef32f72f2be1b8cecce94
heleafreitas <helenldefreitas@gmail.com>
2023-02-27 08:13:14 -0300

    Create index.js

---
+++ js-functions/functional/index.js
@@ -0,0 +1,11 @@
+//First-class function
+
+const sum = (a,b) => a + b;
+const sub = (a,b) => a - b;
+const mult = (a,b) => a * b;
+const div = (a,b) => a / b;
+
+console.log(sum(20,10));
+console.log(sub(20,10));
+console.log(mult(20,10));
+console.log(div(20,10));
