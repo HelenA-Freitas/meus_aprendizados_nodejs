0d80fb0d7f30709165aa00980ddc661e19e3eb35
heleafreitas <helenldefreitas@gmail.com>
2023-03-06 11:58:51 -0300

    Update root.tsx

--- estudo-react-puro/my-app/src/routes/root.tsx
+++ estudo-react-puro/my-app/src/routes/root.tsx
@@ -1,27 +1,90 @@
-import { Box, AppBar, Toolbar, IconButton, Typography } from '@mui/material';
-import MenuIcon from '@mui/icons-material/Menu';
+import { Box, 
+        AppBar, 
+        Toolbar, 
+        Typography, 
+        Checkbox, 
+        Container, 
+        FormControlLabel, 
+        TextField, 
+        createTheme,
+        ThemeProvider} from '@mui/material';
 import Button from '@mui/material/Button';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#FF7F11'
+    },
+    secondary: {
+      main: '#639FAB'
+    }
+  },
+  typography: {
+    fontFamily: "'Courier New', Courier, monospace",
+
+  }
+})
 
 export default function Root() {
     return (
-        <Box sx={{ flexGrow: 1 }}>
+      <ThemeProvider theme={theme}>
+                <><Box sx={{ flexGrow: 2 }}>
         <AppBar position="static">
           <Toolbar>
-            <IconButton
-              size="large"
-              edge="start"
-              color="inherit"
-              aria-label="menu"
-              sx={{ mr: 2 }}
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              News
+            <Typography variant="h6" component="div" sx={{ flexGrow: 5 }}>
+              Loja de Jogos
             </Typography>
-            <Button color="inherit">Login</Button>
+            <Button color="inherit">Criar conta</Button>
           </Toolbar>
         </AppBar>
       </Box>
+      <Container component="main" maxWidth="xs">
+        <Box
+          sx={{
+            marginTop: 25,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            backgroundColor: '#639FAB'
+          }}
+        >
+          <Typography component="h1" variant="h5">
+            Login
+          </Typography>
+          <Box component="form" sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="E-mail"
+              name="email"
+              autoComplete="email"
+              autoFocus />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Senha"
+              type="password"
+              id="password"
+              autoComplete="current-password" />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Lembre de mim" />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color='secondary'
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Entrar
+            </Button>
+          </Box>
+        </Box>
+      </Container></>
+      </ThemeProvider>
     );
   }
